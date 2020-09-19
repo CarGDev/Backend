923711853f36c740aec2b80571e5f2a361bcb78b
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-19 00:29:19 -0500

    changing store

--- Blood-Stream-login/api/components/auth/index.js
+++ Blood-Stream-login/api/components/auth/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// const store = require('../../../store/mysql')
-const store = require('../../../store/dummy')
+const store = require('../../../store/mysql')
+// const store = require('../../../store/dummy')
 const ctrl = require('./controller')
 
 module.exports = ctrl(store)
