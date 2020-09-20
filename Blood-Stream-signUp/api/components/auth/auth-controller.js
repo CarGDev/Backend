ac5e43cc8af6c53a98bd71bb3eb72d3134c22228
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-19 22:48:13 -0500

    Fixing code with standard

--- Blood-Stream-signUp/api/components/auth/auth-controller.js
+++ Blood-Stream-signUp/api/components/auth/auth-controller.js
@@ -1,24 +1,22 @@
-const bcrypt = require('bcrypt');
-const TABLE = 'auth';
+const bcrypt = require('bcrypt')
+const TABLE = 'auth'
 
 module.exports = function (injectedStore = require('../../../store/dummy')) {
+  return {
+    upsert: async (data) => {
+      const authData = {
+        id: data.id
+      }
 
-    return {
-        upsert: async (data) => {
-            const authData = {
-                id: data.id
-            }
-    
-            if (data.username) {
-                authData.username = data.username;
-            }
-    
-            if (data.password) {
-                authData.password = await bcrypt.hash(data.password, 10);
-            }
-    
-            return injectedStore.upsert(TABLE, authData);
-        }
+      if (data.username) {
+        authData.username = data.username
+      }
+
+      if (data.password) {
+        authData.password = await bcrypt.hash(data.password, 10)
+      }
+
+      return injectedStore.upsert(TABLE, authData)
     }
+  }
 }
-
--- Blood-Stream-signUp/api/components/user/user-controller.js
+++ Blood-Stream-signUp/api/components/user/user-controller.js
@@ -1,31 +1,30 @@
-const { nanoid } = require('nanoid');
-const auth = require('../auth');
+const { nanoid } = require('nanoid')
+const auth = require('../auth')
 
-const TABLE = 'user';
+const TABLE = 'user'
 
 module.exports = function (injectedStore = require('../../../store/dummy')) {
+  return {
+    upsert: async (body) => {
+      const user = {
+        name: body.name,
+        username: body.username
+      }
 
-    return {
-        upsert: async (body) => {
-            const user = {
-                name: body.name,
-                username: body.username
-            }
+      if (body.id) {
+        user.id = body.id
+      } else {
+        user.id = nanoid()
+      }
 
-            if (body.id) {
-                user.id = body.id;
-            } else {
-                user.id = nanoid();
-            }
-    
-            if (body.password || body.username) {
-                await auth.upsert({
-                    id: user.id,
-                    username: user.username,
-                    password: body.password,
-                })
-            }
-            return injectedStore.get(TABLE, body)
-        }
+      if (body.password || body.username) {
+        await auth.upsert({
+          id: user.id,
+          username: user.username,
+          password: body.password
+        })
+      }
+      return injectedStore.get(TABLE, body)
     }
+  }
 }
