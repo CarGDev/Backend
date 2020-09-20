c215900f9c2f6aeeb7b39b494fb4d5f74e9b7a86
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-19 23:59:09 -0500

    Create genre games table

--- Blood-Stream-db/lib/genresGames.js
+++ Blood-Stream-db/lib/genresGames.js
@@ -1,1 +1,33 @@
 
+'use strict'
+
+module.exports = function setupGenresGames (genreGamesModel, genreModel, gamesModel) {
+  async function create (uuidGenres, uuidGames, genres) {
+    const genres = await genreModel.findOne({
+      where: { uuidGenres }
+    })
+    const games = await gamesModel.findOne({
+      where: { uuidGames }
+    })
+
+    if (genres && games) {
+      Object.assign(genres, { genresId: gamesRating.id })
+      Object.assign(genres, { gamesId: games.id })
+      const result = await genreGamesModel.create(genres)
+      return result.toJSON()
+    }
+  }
+  function findById (id) {
+    return genreGamesModel.findById(id)
+  }
+
+  function findAll () {
+    return genreGamesModel.findAll()
+  }
+
+  return {
+    create,
+    findById,
+    findAll
+  }
+}
