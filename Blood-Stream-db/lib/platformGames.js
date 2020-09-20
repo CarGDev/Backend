1c5e66d60f40440537d8564ef120d77c3a2b15f3
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-20 00:13:08 -0500

    Platform games table creation

--- Blood-Stream-db/lib/platformGames.js
+++ Blood-Stream-db/lib/platformGames.js
@@ -1,1 +1,33 @@
+'use strict'
+
+module.exports = function setupPlatformGames (gamesModel, platformModel, platformGamesModel) {
+  async function create (uuidGames, uuidPlatform, platformGames) {
+    const games = await gamesModel.findOne({
+      where: { uuidGames }
+    })
+    const platform = await platformModel.findOne({
+      where: { uuidPlatform }
+    })
+
+    if (games && platform) {
+      Object.assign(platformGames, { gamesId: games.id })
+      Object.assign(platformGames, { platformId: platform.id })
+      const result = await platformGamesModel.create(platformGames)
+      return result.toJSON()
+    }
+  }
+  function findById (id) {
+    return platformGamesModel.findById(id)
+  }
+
+  function findAll () {
+    return platformGamesModel.findAll()
+  }
+
+  return {
+    create,
+    findById,
+    findAll
+  }
+}
 
