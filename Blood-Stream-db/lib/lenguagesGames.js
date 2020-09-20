bbc507507b6d54debf4a3c86f3ccdd15e0ab6bc6
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-20 00:17:40 -0500

    Create lenguage table

--- Blood-Stream-db/lib/lenguagesGames.js
+++ Blood-Stream-db/lib/lenguagesGames.js
@@ -1,1 +1,34 @@
+'use strict'
+
+module.exports = function setupLanguagesGames (gamesModel, lenguagesModel, lenguagesGamesModel) {
+  async function create (uuidGames, uuidLenguages, lenguagesGames) {
+    const games = await gamesModel.findOne({
+      where: { uuidGames }
+    })
+    const lenguages = await lenguagesModel.findOne({
+      where: { uuidLenguages }
+    })
+
+    if (games && lenguages) {
+      Object.assign(lenguagesGames, { gamesId: games.id })
+      Object.assign(lenguagesGames, { lenguagesId: lenguages.id })
+      const result = await lenguagesGamesModel.create(lenguagesGames)
+      return result.toJSON()
+    }
+  }
+  function findById (id) {
+    return lenguagesGamesModel.findById(id)
+  }
+
+  function findAll () {
+    return lenguagesGamesModel.findAll()
+  }
+
+  return {
+    create,
+    findById,
+    findAll
+  }
+}
+
 
