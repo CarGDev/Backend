d712a50a74297fe5c0d63c7b521e7882ab8b2311
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-20 00:07:58 -0500

    userRating creation table

--- Blood-Stream-db/lib/userRating.js
+++ Blood-Stream-db/lib/userRating.js
@@ -1,1 +1,32 @@
 'use strict'
+
+module.exports = function setupUserRating (userRatingModel, gamesRatingModel, usersModel) {
+  async function create (uuidGamesRating, uuidUsers, userRating) {
+    const gamesRating = await gamesRatingModel.findOne({
+      where: { uuidGamesRating }
+    })
+    const users = await usersModel.findOne({
+      where: { uuidUsers }
+    })
+
+    if (gamesRating && users) {
+      Object.assign(userRating, { gamesRatingId: gamesRating.id })
+      Object.assign(userRating, { usersId: users.id })
+      const result = await userRatingModel.create(userRating)
+      return result.toJSON()
+    }
+  }
+  function findById (id) {
+    return userRatingModel.findById(id)
+  }
+
+  function findAll () {
+    return userRatingModel.findAll()
+  }
+
+  return {
+    create,
+    findById,
+    findAll
+  }
+}
