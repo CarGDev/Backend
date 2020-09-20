c4316e9a801a60d22d6dc2229d16aaf7214ee944
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-20 01:34:17 -0500

    Fixing error in database

--- Blood-Stream-db/setup.js
+++ Blood-Stream-db/setup.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const db = require('./')
+const db = require('./index')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
 const config = require('./config')
 
 const prompt = inquirer.createPromptModule()
 
-async function setup () {
+async function setup (value) {
   let answer = false
   process.argv.forEach((val) => {
     if (val === '--yes' || val === '-y') {
@@ -27,15 +27,15 @@ async function setup () {
       return console.log(chalk.green('Nothing happened :)'))
     }
   }
-  await db(config(true)).catch(handleFatalError)
-
-  console.log(`${chalk.bgGreen.white('[Connected]:')} Success!`)
+  await db(config(value)).catch(handleFatalError)
+  
+  console.log(`${chalk.bgGreen.black('[Connected]:')} Success!`)
   process.exit(0)
 }
 
 function handleFatalError (err) {
-  console.error(`${chalk.bgRed.white('[fatal error]:')} ${err.message}`)
-  console.error(`${chalk.bgRed.white('[Error]:')} ${err.stack}`)
+  console.error(`${chalk.bgRed.black('[fatal error]:')} ${err.message}`)
+  console.error(`${chalk.bgRed.black('[Error]:')} ${err.stack}`)
   process.exit(1)
 }
 
