14d2dde48685c29af212068316349ee5efb406bc
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-22 10:13:46 -0500

    Changing rute in db

--- Blood-Stream-login/api/components/user/index.js
+++ Blood-Stream-login/api/components/user/index.js
@@ -2,7 +2,7 @@
 
 // const store = require('../../../store/mysql')
 // const store = require('../../../store/dummy')
-const store = require('configuration')
+const store = require('../../../../Blood-Stream-db/setup')
 const ctrl = require('./controller')
 
 module.exports = ctrl(store)
