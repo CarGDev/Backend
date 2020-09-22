7f66f09fa2c60c9ad77fe650466de402256dbeda
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-22 16:21:38 -0500

    Modifiying the database

--- Blood-Stream-login/api/components/auth/index.js
+++ Blood-Stream-login/api/components/auth/index.js
@@ -1,9 +1,6 @@
 'use strict'
 
-// const store = require('../../../store/mysql')
-
-const store = require('../../../../config/config')
-// const store = require('../../../store/dummy')
+const store = require('../../../store/db')
 const ctrl = require('./controller')
 
 module.exports = ctrl(store)
---
+++ Blood-Stream-login/store/db.js
@@ -0,0 +1,44 @@
+const utils = require('../../Blood-Stream-db/utils/index')
+const config = require('../../config/config')
+const db = require('../../Blood-Stream-db/setup')
+
+module.exports = async function () {
+  const { 
+    Message,
+    Password,
+    Users,
+    GamesCollection,
+    Contact,
+    AccessRol,
+    Platform,
+    PlatformGames,
+    Lenguages,
+    LenguagesGames,
+    Genres,
+    GenresGames,
+    Games,
+    UserRating,
+    GamesRating,
+    GameRating
+   } = await db(config(false)).catch(utils.handleFatalError)
+   
+   return {
+    Message,
+    Password,
+    Users,
+    GamesCollection,
+    Contact,
+    AccessRol,
+    Platform,
+    PlatformGames,
+    Lenguages,
+    LenguagesGames,
+    Genres,
+    GenresGames,
+    Games,
+    UserRating,
+    GamesRating,
+    GameRating
+   }
+}
+ 
