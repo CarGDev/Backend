a54c77985c783fd2e63edcec9a9e475d30770e9c
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-22 10:52:53 -0500

    Adding module

--- Blood-Stream-login/api/components/user/index.js
+++ Blood-Stream-login/api/components/user/index.js
@@ -2,7 +2,7 @@
 
 // const store = require('../../../store/mysql')
 // const store = require('../../../store/dummy')
-const store = require('../../../../Blood-Stream-db/setup')
+const store = require('databasePostgres')
 const ctrl = require('./controller')
 
 module.exports = ctrl(store)
