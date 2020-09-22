20730201da7ba504a1dfc5dd24858e9dc7149b86
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-22 14:05:18 -0500

    Fixing dependencies

--- Blood-Stream-login/api/components/auth/index.js
+++ Blood-Stream-login/api/components/auth/index.js
@@ -2,7 +2,7 @@
 
 // const store = require('../../../store/mysql')
 
-const store = require('databasePostgres')
+const store = require('../../../../config/config')
 // const store = require('../../../store/dummy')
 const ctrl = require('./controller')
 
