4ad06c47edc927f0353393ac9b8ebae272bd867d
cargdev <ingecarlos.gutierrez@gmail.com>
2020-09-22 11:56:58 -0500

    Test

--- Blood-Stream-login/api/components/user/index.js
+++ Blood-Stream-login/api/components/user/index.js
@@ -1,8 +1,8 @@
 'use strict'
 
-// const store = require('../../../store/mysql')
+const store = require('../../../store/mysql')
 // const store = require('../../../store/dummy')
-const store = require('databasePostgres')
+// const store = require('databasePostgres')
 const ctrl = require('./controller')
 
 module.exports = ctrl(store)
