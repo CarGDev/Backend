dd7a9704d68b30c3b3b09287c8dd39bb03c4a95f
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-01 21:34:37 -0500

    Adding configurations

--- Blood-Stream-login/index.js
+++ Blood-Stream-login/index.js
@@ -8,6 +8,7 @@ const config = require('./config')
 const user = require('./api/components/user/network')
 const errors = require('./network/errors')
 const app = express()
+const conf = require('../config/config')
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -23,5 +24,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 app.use(errors)
 
 app.listen(config.api.port, () => {
+  console.log(conf(false).database)
+  console.log(conf(false).hostname)
   console.log(`Api escuchando en el puerto ${config.api.port}`)
 })
