60c5d5f42f359fe0d128f1205a7e94db19de6e6d
cargdev <ingecarlos.gutierrez@gmail.com>
2020-10-02 16:14:56 -0500

    Taking out the console log in the config file

--- config/config.js
+++ config/config.js
@@ -26,9 +26,7 @@ module.exports = function config (configExtra) {
     }
   }
 
-  console.log(process.env.NODE_ENV)
   if (process.env.NODE_ENV === 'production') {
-    console.log('hola')
     Object.assign(config, {
       dialectOptions: {
         ssl: {
